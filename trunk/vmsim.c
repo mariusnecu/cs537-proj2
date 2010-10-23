4c572779027a6b20356ce0eeb04e8d1f7e958e6c
citizen822 <citizen822@5530b9a4-2029-16e0-e5f6-e25702553fe8>
2010-10-23 18:51:27 +0000

    Parses addresses correctly, does not store them yet.

--- trunk/vmsim.c
+++ trunk/vmsim.c
@@ -3,14 +3,41 @@
  * Primary source file for the Virtual Memory Simulator
  */
 
-
 #include <stdio.h>
+#include <stdlib.h>
 #include "tlbtest.h"
 #include "wsstest.h"
 #include "pttest.h"
 #include "prtest.h"
 
+unsigned long parseNext(FILE* fp);
+
 int main(int argc, char *argv[]) {
-    
+    // Open the file.
+	// vimsim tlbtest tracefile pagesize tblsize
+	int store, i;
+	char* end;
+	char arr[33];
+	unsigned long long trace_address;
+	unsigned long address;
+	
+	FILE *fp = fopen(argv[2], "r");
+
+	while ((store = fgetc(fp)) != EOF)
+	{
+		if (store == '\n')
+		{
+			arr[i] = store;
+			// Convert it, add it.
+			trace_address = strtoq(arr, &end, 16);
+			address = (unsigned long) trace_address & 0xffffffff;
+			printf("Address added: %lx\n", address);
+			i = 0;
+		}
+		else {
+			arr[i] = store;
+			i++;
+		}
+	}
     return 0;
 }
