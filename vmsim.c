da6972f801ca780e32a96c34a8f352ff2bbb94b1
citizen822 <citizen822@5530b9a4-2029-16e0-e5f6-e25702553fe8>
2010-10-23 21:03:52 +0000

    Bugs fixed with previous version of parsing pintrace.

--- vmsim.c
+++ vmsim.c
@@ -9,15 +9,18 @@
 #include "wsstest.h"
 #include "pttest.h"
 #include "prtest.h"
+#include <string.h>
 
 unsigned long parseNext(FILE* fp);
 
 int main(int argc, char *argv[]) {
     // Open the file.
 	// vimsim tlbtest tracefile pagesize tblsize
-	int store, i;
+	int store, i, j;
 	char* end;
 	char arr[33];
+	char first[20];
+	char second[20];
 	unsigned long long trace_address;
 	unsigned long address;
 	
@@ -28,10 +31,39 @@ int main(int argc, char *argv[]) {
 		if (store == '\n')
 		{
 			arr[i] = store;
-			// Convert it, add it.
-			trace_address = strtoq(arr, &end, 16);
+			
+			j = 0;
+			// Pad unneeded with spaces to easily tokenize.
+			while (arr[j] != '\n')
+			{
+				if (arr[j] == ':' || arr[j] == 'W' || arr[j] == 'R')
+				{
+					arr[j] = 0x20;
+				}
+				j++;
+			}
+			
+			char* cpy = (char *)malloc(sizeof(arr));
+			strcpy(cpy, arr);
+
+			// First address.
+			char* tok = strtok(cpy, "\t ");
+			strcpy(first, tok);
+			
+			// Second address.
+			tok = strtok(NULL, "\t ");
+			strcpy(second, tok);
+			
+			// Convert to 32bit.
+			trace_address = strtoq(first, &end, 16);
 			address = (unsigned long) trace_address & 0xffffffff;
 			printf("Address added: %lx\n", address);
+			
+			trace_address = strtoq(second, &end, 16);
+			address = (unsigned long) trace_address & 0xffffffff;
+			printf("Address added: %lx\n", address);
+			
+			free(cpy);
 			i = 0;
 		}
 		else {
