866a5d66cf58f028a77dd84e39714038a78299c5
citizen822 <citizen822@5530b9a4-2029-16e0-e5f6-e25702553fe8>
2010-10-24 22:57:54 +0000

    tlb implementation almost completed.  LRU algorithm not implemented quite yet.

--- vmsim.h
+++ vmsim.h
@@ -1,3 +1,5 @@
+#import <math.h>
+
 struct TLB
 {
 	int size;
@@ -8,11 +10,11 @@ struct TLB
 struct VPN
 {
 	unsigned long number;
-	int time;
+	int hits;
 };
 
 struct TLB* createTLB(int size, int pageSize);
-void insert(struct TLB* tlb, struct VPN* vpn, int rollingMiss);
+void insert(struct TLB* tlb, struct VPN* vpn, int *rollingMiss);
 
 struct TLB* createTLB(int size, int pageSize)
 {
@@ -23,16 +25,59 @@ struct TLB* createTLB(int size, int pageSize)
 	int i;
 	for (i = 0; i<size; i++)
 	{
-		// Null out the TLB initially to check fullness.
-		ptr->arr[i] = NULL;
+		struct VPN* vpn = (struct VPN*)malloc(sizeof(struct VPN));
+		vpn->number = 0;
+		vpn->hits = 0;
+		ptr->arr[i] = vpn;
 	}
+	
+	return ptr;
 }
 
 // This function will do all of the heavy lifting for the TLB.
 // Specify which tlb the vpn should be inserted into, as well as
 // a counter (which should be called by reference) to keep track
 // of total misses in the tlb thus far.
-void insert(struct TLB* tlb, struct VPN* vpn, int rollingMiss)
+void insert(struct TLB* tlb, struct VPN* vpn, int *rollingMiss)
 {
+	int j;
+	int checkHit, slotFound;
 	
+	// virtNum is the number of bits to be masked with vpn->num.
+/*	unsigned long virtNum = logl(2^(tlb->pageSize))/logl(2);
+	unsigned long mask = vpn->number & virtNum;*/
+	
+	for (j = 0; j<tlb->size; j++)
+	{
+		if (tlb->arr[j] != 0)
+		{
+			if (tlb->arr[j]->number == vpn->number)
+			{
+				// Addresses is in the TLB.
+				(tlb->arr[j]->hits)++;
+				checkHit = 1;
+				break;
+			}
+		}
+	}
+	if (checkHit == 0)
+	{
+		// We must insert it now into the table.
+		for (j = 0; j<tlb->size; j++)
+		{
+			if (tlb->arr[j]->number == 0)
+			{
+				free(tlb->arr[j]);
+				tlb->arr[j] = vpn;
+				slotFound = 1;
+				break;
+			}
+		}
+		if (slotFound == 0)
+		{
+			// Page replacement algorithm here.
+			
+		}
+		*rollingMiss = *rollingMiss + 1;
+	}
 }
