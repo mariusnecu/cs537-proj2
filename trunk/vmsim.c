2c0d33c04a868c19610e81bbd0063d015689fa6b
citizen822 <citizen822@5530b9a4-2029-16e0-e5f6-e25702553fe8>
2010-10-27 02:39:53 +0000

    New linked list implementation for every line in the pintrace; find it in vmsim.h.  P0 is finished; P1 is 98% done.

--- trunk/vmsim.c
+++ trunk/vmsim.c
@@ -15,6 +15,11 @@ int main(int argc, char *argv[]) {
 
 	unsigned long long instAddr;
 	unsigned long long dataAddr;
+	struct linkedAddr* llAddr = createLinkedAddr();
+	
+	// Use 'head' to scan the linked list for every line in the pin trace.
+	// Implementation for linkedAddr is in vmsim.h
+	// struct linkedAddr* head = llAddr;
 	
 	char* rw = (char *)malloc(sizeof(char));
 	char* line = (char *)malloc(33 * sizeof(char));
@@ -24,20 +29,33 @@ int main(int argc, char *argv[]) {
 	int pageSize = atoi(argv[3]);
 	int tlbSize = atoi(argv[4]);
 	
+	// Parse and store each line.
 	while (strcmp(fgets(line, 33, fp), "#eof") != 0)
 	{
 		sscanf(line, "0x%llx: %s 0x%llx", &instAddr, rw, &dataAddr);
 		
 		// Populate global structure for tests to use
 		// (all addresses found so far)
-		
+		struct addr* address = createAddr(instAddr, rw, dataAddr);
+		addToEnd(llAddr, address);
 		//-------------------------------------------
 	}
 	
 	if (strcmp(testChoice, "tlbtest") == 0)
 	{
+		// fp needs to be reset before being used again.
 		fp = fopen(argv[2], "r");
 		tlbtest(fp, tlbSize, pageSize);
+	}
+	
+	if (strcmp(testChoice, "pttest") == 0)
+	{
+		
+	}
+	
+	if (strcmp(testChoice, "wsstest") == 0)
+	{
+		
 	}
 	
     return 0;
