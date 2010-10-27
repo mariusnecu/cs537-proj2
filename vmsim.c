58f88b7b055e7258746cf20d2b4ee47f0145f052
citizen822 <citizen822@5530b9a4-2029-16e0-e5f6-e25702553fe8>
2010-10-27 01:56:25 +0000

    Substantial changes made to how vmsim.c works, it should be pretty clear how things work now with it.  There's one warning I can't seem to solve, but it still compiles and runs correctly.

--- vmsim.c
+++ vmsim.c
@@ -5,7 +5,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "tlbtest.h"
 #include "vmsim.h"
 #include "wsstest.h"
 #include "pttest.h"
@@ -14,52 +13,32 @@
 
 int main(int argc, char *argv[]) {
 
-    // Open the file.
-	int store, i, j;
-	int rollingMiss = 0;
-	int pageSize, tlbSize;
-	char* end;
-	char* line = (char *)malloc(33 * sizeof(char));
 	unsigned long long instAddr;
 	unsigned long long dataAddr;
+	
 	char* rw = (char *)malloc(sizeof(char));
-	struct VPN* vpn;
-	unsigned long address;
+	char* line = (char *)malloc(33 * sizeof(char));
 	
 	char* testChoice = argv[1];
 	FILE *fp = fopen(argv[2], "r");
-	pageSize = atoi(argv[3]);
-	tlbSize = atoi(argv[4]);
-	
-	// Create the tlb.
-	struct TLB* tlb = createTLB(tlbSize, pageSize);
+	int pageSize = atoi(argv[3]);
+	int tlbSize = atoi(argv[4]);
 	
 	while (strcmp(fgets(line, 33, fp), "#eof") != 0)
 	{
 		sscanf(line, "0x%llx: %s 0x%llx", &instAddr, rw, &dataAddr);
-			
-		// VPN to be inserted.
-		vpn = createVPN();
-			
-		// Convert to 32bit.
-		address = (int) instAddr & 0xffffffff;
-		vpn->number = address;
-		insertIntoTLB(tlb, vpn, &rollingMiss);
-		printf("Address added: %lx\n", address);
-			
-		address = (int) dataAddr & 0xffffffff;
-		vpn->number = address;
-		insertIntoTLB(tlb, vpn, &rollingMiss);
-		printf("Address added: %lx\n", address);
-			
-		printf("Read/Write: %s\n", rw);
+		
+		// Populate global structure for tests to use
+		// (all addresses found so far)
+		
+		//-------------------------------------------
 	}
-
-	// Count number of entries in evicted and in tlb.
-	printf("\nTLB misses: %d\n", rollingMiss);
 	
-	int totalUnique = countUnique(tlb);
-	printf("VPNs: %d\n", totalUnique);
+	if (strcmp(testChoice, "tlbtest") == 0)
+	{
+		fp = fopen(argv[2], "r");
+		tlbtest(fp, tlbSize, pageSize);
+	}
 	
     return 0;
 }
