92b00ebce7d9eba962366a791b1182eb38ef9f58
jeremy.weiss <jeremy.weiss@5530b9a4-2029-16e0-e5f6-e25702553fe8>
2010-11-02 16:00:40 +0000

    Commented

--- trunk/wsstest.c
+++ trunk/wsstest.c
@@ -5,16 +5,20 @@
 #include<unistd.h>
 #include"vmsim.h"
 
+//Returns the page by shifting off the offset
 int page(unsigned int address, int pagesize) {
   int offset = (int)ceil(log2((double)pagesize));
   return address >> offset;
 }
 
+/*
+ * wsstest prints out the average page size of the working set
+ */
 void wsstest(int pagesize, int windowsize) {
   mem_loc *loc = mem_head;
   int c=0,cmod,i, runningtotal=0, sum, sums = 0;
   mem_loc **window;
-  int same_as[windowsize];
+  int same_as[windowsize];             //-2=unitialized, -1=unique, n=highest match
   //  FILE *TRACE;
 
   for(i=0;i<windowsize;i++) {
@@ -30,20 +34,30 @@ void wsstest(int pagesize, int windowsize) {
     cmod = c % windowsize;
     window[cmod] = loc;
     same_as[cmod]=-1; //Enters, now test for uniqueness
+
     //    fprintf(TRACE,"Time %i\n",c/2);
     //    fprintf(TRACE,"placing 0x%x",page(window[cmod]->address,pagesize));
+
+    //Find if a match exists
     for(i=c-1;i>=c-windowsize && i>=0;i--) {
+
       if(window[i%windowsize]!=NULL
 	 && page(window[cmod]->address,pagesize)
 	    == page(window[i%windowsize]->address,pagesize)) {
+
 	same_as[cmod]=i;
+
 	//	fprintf(TRACE, ", same as %i in %i\n", page(window[i%windowsize]->address,pagesize),i);
+
 	break;
       }
     }
+
 //    for(i=0;i<windowsize;i++) {
 //      fprintf(TRACE,"__%i\n",same_as[i]);
 //    }
+
+    //Calculate the window size
     if(c%2==1 && (c>windowsize || loc->next==NULL)) {
       sum=0;
       for(i=0;i<windowsize;i++) {
@@ -55,8 +69,12 @@ void wsstest(int pagesize, int windowsize) {
       runningtotal+=sum;
       sums++;
     }
+
+    //Proceed to next address
     c++;
     loc = loc->next;
   }
+
   printf("WS Size: %i\n",runningtotal/sums);
+  free(window);
 }
