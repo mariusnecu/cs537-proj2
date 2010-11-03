ac9b444520fcc809cff20f844ee52887e72f3dc2
ovaninza@hotmail.com <ovaninza@hotmail.com@5530b9a4-2029-16e0-e5f6-e25702553fe8>
2010-11-03 00:37:36 +0000

    header comment and indentation

--- trunk/prtest.h
+++ trunk/prtest.h
@@ -1,3 +1,23 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Project 2 - Virtual Memory
+// Section 2
+//
+// Authors:
+//  Troy Cornell
+//  Richard Joiner
+//  Zach Ovanin
+//  Jeremy Weiss
+//
+// Files:
+//  prtest.c, prtest.h
+//  pttest.c, pttest.h
+//  tlbtest.c, tlbtest.h
+//  wsstest.c, wsstest.h
+//  vmsim.c, vmsim.h
+//
+////////////////////////////////////////////////////////////////////////////////
+
 void prtest_2nd_fifo(int pagesize, int numframes);
 void prtest_fifo(int pagesize, int numframes);
 
