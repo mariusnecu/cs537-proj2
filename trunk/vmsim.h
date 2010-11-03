fdd6a71c93861669ae9d0472245901475058d477
ovaninza@hotmail.com <ovaninza@hotmail.com@5530b9a4-2029-16e0-e5f6-e25702553fe8>
2010-11-03 00:38:55 +0000

    header comment and indentation

--- trunk/vmsim.h
+++ trunk/vmsim.h
@@ -1,6 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Project 2 - Virtual Memory
+// Section 2
+//
+// Authors:
+//  Troy Cornell
+//  Richard Joiner
+//  Zach Ovanin
+//  Jeremy Weiss
+//
+// Files:
+//  prtest.c, prtest.h
+//  pttest.c, pttest.h
+//  tlbtest.c, tlbtest.h
+//  wsstest.c, wsstest.h
+//  vmsim.c, vmsim.h
+//
+////////////////////////////////////////////////////////////////////////////////
+
 typedef struct _mem_loc {
-    struct _mem_loc *next;
-    unsigned int address;
+  struct _mem_loc *next;
+  unsigned int address;
 } mem_loc;
 
 mem_loc *mem_head;
