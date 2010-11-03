8e4fd0607f4e02e45bb1b1d252b306ff6de2a412
ovaninza <ovaninza@hotmail.com>
2010-11-03 00:38:06 +0000

    header comment and indentation

--- pttest.h
+++ pttest.h
@@ -1,3 +1,23 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Project 2 - Virtual Memory
+// Section 2
+//
+// Authors:
+//  Troy Cornell
+//  Richard Joiner
+//  Zach Ovanin
+//  Jeremy Weiss
+//
+// Files:
+//  prtest.c, prtest.h
+//  pttest.c, pttest.h
+//  tlbtest.c, tlbtest.h
+//  wsstest.c, wsstest.h
+//  vmsim.c, vmsim.h
+//
+////////////////////////////////////////////////////////////////////////////////
+
 /* pttest.h
  *
  * Header file containing function signatures for pttest.c.
@@ -7,21 +27,21 @@ struct _page_table;
 
 // A page table entry pointing to either a physical frame or another page table.
 struct _entry {
-    int vpn;
-    int to_frame;
-    struct _page_table *to_pt;
+  int vpn;
+  int to_frame;
+  struct _page_table *to_pt;
 };
 
 // A page table containing a pointer to a list of entries and a count of how
 // full the table it is
 struct _page_table {
-    struct _entry **entries;
-    int frame_number;
+  struct _entry **entries;
+  int frame_number;
 };
 
 struct _frame_list {
-    struct _frame_list *next;
-    struct _entry *ent;
+  struct _frame_list *next;
+  struct _entry *ent;
 };
 
 typedef struct _page_table page_table;
